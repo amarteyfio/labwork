2e4c223669448af6b61b4d2d244af9584661007d
amarteyfio <amarteyfio>
2022-10-19 17:55:29 +0100

    water

---
+++ Settings/db_cred.php
@@ -0,0 +1,17 @@
+<?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("mysql://bbb7d7a4ab2a24:690a2818@us-cdbr-east-06.cleardb.net/heroku_c01494770f15ced?reconnect=true"));
+$cleardb_server = $cleardb_url["us-cdbr-east-06.cleardb.net"];
+$cleardb_username = $cleardb_url["bbb7d7a4ab2a24"];
+$cleardb_password = $cleardb_url["690a2818"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+
+
+define('DB_SERVER', $cleardb_url["us-cdbr-east-06.cleardb.net"]);
+define('DB_USERNAME', $cleardb_url["bbb7d7a4ab2a24"]);
+define('DB_PASSWORD', $cleardb_url["690a2818"]);
+define('DB_NAME', substr($cleardb_url["path"],1));
+
+?>

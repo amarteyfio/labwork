d7c791e2a1daaa71ced420b7eb06903bf5713330
amarteyfio <amarteyfio>
2022-10-19 19:42:24 +0100

    login

---
+++ Login/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login Form</h1>
+    <form method="POST" action="#">
+        <input type="text" placeholder = "Enter your email">
+        <br>
+        <br>
+        <input type="password" placeholder="Enter your Password">
+        <br>
+        <input type="button" value="Login">
+    </form>
+</body>
+</html>

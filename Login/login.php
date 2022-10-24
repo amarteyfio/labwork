f801397cf654ba281a8b79e45e494cecce006992
amarteyfio <amarteyfio>
2022-10-24 16:29:28 +0100

    undo

--- Login/login.php
+++ Login/login.php
@@ -24,7 +24,7 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     <title>Login</title>
 </head>
 <body>
-    <!--
+    
     <h1>Login Form</h1>
     <form method="POST" action="loginprocess.php">
         <input type="email" placeholder = "Enter your Email" name="email" required>
@@ -34,61 +34,7 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
         <br>
         <br>
         <input type="Submit" value="Login" name="login" >
-    </form>-->
-<form>
-  <!-- Email input -->
-  <div class="form-outline mb-4">
-    <input type="email" id="form2Example1" class="form-control"  name="email" required/>
-    <label class="form-label" for="form2Example1">Email address</label>
-  </div>
-
-  <!-- Password input -->
-  <div class="form-outline mb-4">
-    <input type="password" id="form2Example2" class="form-control" name="pass"/>
-    <label class="form-label" for="form2Example2">Password</label>
-  </div>
-
-  <!-- 2 column grid layout for inline styling -->
-  <div class="row mb-4">
-    <div class="col d-flex justify-content-center">
-      <!-- Checkbox -->
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
-        <label class="form-check-label" for="form2Example31"> Remember me </label>
-      </div>
-    </div>
-
-    <div class="col">
-      <!-- Simple link -->
-      <a href="#!">Forgot password?</a>
-    </div>
-  </div>
-
-  <!-- Submit button -->
-  <input type="submit" class="btn btn-primary btn-block mb-4" value="login" name="login">
-
-  <!-- Register buttons -->
-  <div class="text-center">
-    <p>Not a member? <a href="#!">Register</a></p>
-    <p>or sign up with:</p>
-    <button type="button" class="btn btn-link btn-floating mx-1">
-      <i class="fab fa-facebook-f"></i>
-    </button>
-
-    <button type="button" class="btn btn-link btn-floating mx-1">
-      <i class="fab fa-google"></i>
-    </button>
-
-    <button type="button" class="btn btn-link btn-floating mx-1">
-      <i class="fab fa-twitter"></i>
-    </button>
-
-    <button type="button" class="btn btn-link btn-floating mx-1">
-      <i class="fab fa-github"></i>
-    </button>
-  </div>
-</form>
-
+    </form>
 </body>
 </html>
 

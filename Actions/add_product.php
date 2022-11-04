8c712ac3c015844b3c9ec943f4ad0d2809ffa704
amarteyfio <amarteyfio>
2022-11-04 21:58:43 +0000

    ad

--- Actions/add_product.php
+++ Actions/add_product.php
@@ -16,6 +16,7 @@ require("../Controller/product_controller.php");
 
 
 <?php
+//
     if(isset($_POST['submit'])){
         $cat = $_POST['cat'];
         $brand = $_POST['brand'];
@@ -29,8 +30,9 @@ require("../Controller/product_controller.php");
         echo "Something went wrong";
     }
 
+    var_dump($cat)
 
-    addprod_ctr($cat,$brand,$tit,$prc,$desc,$img,$keys);
+    addprod_ctr($cat,$brand,$tit,$prc,$desc,$img,$keys);//add to next page 
 
     header("Location: ../View/product.php");
 

ea079c81c95eeeb18812f8d13a532da20222f9b0
amarteyfio <amarteyfio>
2022-11-07 20:08:33 +0000

    gg

---
+++ View/single_product.php
@@ -0,0 +1,17 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: ../Login/login.php");
+    exit;
+}
+
+//require controller
+require("../Controller/product_controller.php");
+
+$id = $_GET['id'];//get id
+
+$product = selprod_ctr($id);
+?>

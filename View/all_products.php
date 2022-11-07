60725b63a2723be267b09aa06fb405a411ec46b9
amarteyfio <amarteyfio>
2022-11-07 19:36:03 +0000

    fixes

--- View/all_products.php
+++ View/all_products.php
@@ -75,7 +75,7 @@ $products = selall_ctr("products");
                     <div class="col mb-5">
                         <div class="card h-100">
                             <!-- Product image-->
-                            <img class="card-img-top" src="<?php echo $product['product_image'];?>" alt="..." />
+                            <img class="card-img-top" src="<?php echo $product['product_image'];?>" alt="<?php echo $product['product_title']; ?>" width="300" height="450" />
                             <!-- Product details-->
                             <div class="card-body p-4">
                                 <div class="text-center">

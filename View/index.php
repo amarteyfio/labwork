af32bd7102fa71d2da2a0ca78b6e9e89f52a6493
amarteyfio <amarteyfio>
2022-11-08 21:55:57 +0000

    search

--- View/index.php
+++ View/index.php
@@ -16,6 +16,8 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js"></script>
     <title>Document</title>
 </head>
 <body>
@@ -35,11 +37,11 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
         if($_SESSION['role'] == 1)
         {
-            echo "<a href='$logout'>LOGOUT</a> | <a href='$register'>REGISTER</a> | <a href = '$brand'>BRAND</a> | <a href = '$category'>CATEGORY</a> | <a href='$product'>ADD PRODUCT</a> | <a href='$allprod'>ALL PRODUCTS</a> | <br><br><form action = '../Actions/product_functions.php' method 'POST'> <input type = 'text' name = 'term' id = 'search_term' placeholder = 'Search for a product'><input type = 'submit' name = 'search' id = 'search' value = 'Search'></form>";
+            echo "<a href='$logout'>LOGOUT</a> | <a href='$register'>REGISTER</a> | <a href = '$brand'>BRAND</a> | <a href = '$category'>CATEGORY</a> | <a href='$product'>ADD PRODUCT</a> | <a href='$allprod'>ALL PRODUCTS</a> | <br><br><form action = '../Actions/product_functions.php' method 'POST'> <input type = 'text' name = 'term' id = 'search_term' placeholder = 'Search for a product'><input type = 'submit' name = 'search' id = 'search' value = 'Search<i class='fa fa-search' aria-hidden='true'></i>'></form>";
         }
         else
         {
-           echo "<a href='$logout'>LOGOUT</a> | <a href='$register'>REGISTER</a> | <a href='$product'>ADD PRODUCT</a> | <a href='$allprod'>ALL PRODUCTS</a> | <br><br><form action = '../Actions/product_functions.php' method 'POST'> <input type = 'text' name = 'term' id = 'search_term' placeholder = 'Search for a product'><input type = 'submit' name = 'search' id = 'search' value = 'Search'></form>";
+           echo "<a href='$logout'>LOGOUT</a> | <a href='$register'>REGISTER</a> | <a href='$product'>ADD PRODUCT</a> | <a href='$allprod'>ALL PRODUCTS</a> | <br><br><form action = '../Actions/product_functions.php' method 'POST'> <input type = 'text' name = 'term' id = 'search_term' placeholder = 'Search for a product'><input type = 'submit' name = 'search' id = 'search' value = 'Search<i class='fa fa-search' aria-hidden='true'></i>'></form>";
         }
 
         ?>

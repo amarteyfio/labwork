84ba79d7e47476ee4685a64c68ec746ae55c45fe
amarteyfio <amarteyfio>
2022-11-10 14:58:50 +0000

    gg

--- View/all_products.php
+++ View/all_products.php
@@ -8,6 +8,12 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     exit;
 }
 
+// Page is set to home (home.php) by default, so when the visitor visits that will be the page they see.
+$page = isset($_GET['page']) && file_exists($_GET['page'] . '.php') ? $_GET['page'] : 'home';
+// Include and show the requested page
+include $page . '.php';
+
+
 //require controller
 require("../Controller/product_controller.php");
 
